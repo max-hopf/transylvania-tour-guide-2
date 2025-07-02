c2f40c629b50da65e01f510761ec6856178c7bc5
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2025-07-02 15:22:41 +0300

    Add lazy-loading to tour page gallery
    to improve page and image loading speed

--- src/components/TourPageGallery.vue
+++ src/components/TourPageGallery.vue
@@ -20,7 +20,7 @@
             marginRight: idx < row.length - 1 ? gap + 'px' : 0
           }"
         >
-          <img :src="img.src" :alt="img.alt || ''" :style="{width: '100%', height: '100%', objectFit: 'cover'}" />
+          <img :src="img.src" :alt="img.alt || ''" loading="lazy" :style="{width: '100%', height: '100%', objectFit: 'cover'}" />
         </div>
       </div>
     </div>
