b0d5071328d9e60ace76fdf94eacf804aadda858
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2025-08-04 21:26:57 +0300

    Update base URL for custom domain

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/transylvania-tour-guide-2/',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
